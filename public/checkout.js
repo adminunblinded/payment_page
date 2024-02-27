cb8af8bd974063425c400705a38d6740288e672e
adminunblinded <158335079+adminunblinded@users.noreply.github.com>
2024-02-26 19:53:19 -0500

    Create checkout.js

---
+++ public/checkout.js
@@ -0,0 +1,27 @@
+// Initialize Stripe.js
+var stripe = Stripe('pk_test_dnxIicCufTTvDdOvJJQmsGIt');
+
+// Create an instance of Elements
+var elements = stripe.elements();
+
+// Create and mount the card number Element
+var cardNumberElement = elements.create('cardNumber');
+cardNumberElement.mount('#cardNumberElement');
+
+// Create and mount the CVC Element
+var cardCvcElement = elements.create('cardCvc');
+cardCvcElement.mount('#cardCvcElement');
+
+// Create and mount the expiration date Element
+var cardExpiryElement = elements.create('cardExpiry');
+cardExpiryElement.mount('#cardExpiryElement');
+
+// Function to toggle recurring payment options
+function toggleRecurringOptions() {
+  var recurringOptions = document.getElementById('recurringOptions');
+  if (document.getElementById('recurringPayments').checked) {
+    recurringOptions.style.display = 'block';
+  } else {
+    recurringOptions.style.display = 'none';
+  }
+}
