dc5531aabb7474c056481bb3f89df717533ae0c4
adminunblinded <158335079+adminunblinded@users.noreply.github.com>
2024-02-28 16:49:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -166,15 +166,16 @@ async function processPayment(token, amount, email, firstName, lastName, product
 
             const start = new Date(startDate + "T00:00:00Z"); // Ensures correct parsing with time set to 00:00:00 UTC
             const endDate = new Date(start);
-
+            const numPayments = parseInt(numberOfPayments, 10);
+          
             // Correctly add numberOfPayments months to the startDate
-            endDate.setMonth(endDate.getMonth() + numberOfPayments);
+            endDate.setMonth(endDate.getMonth() + numPayments);
 
             // Stripe requires UNIX timestamps in seconds
             const billing_cycle_anchor = Math.floor(start.getTime() / 1000);
             const cancel_at = Math.floor(endDate.getTime() / 1000); // Ensure this is the end date UNIX timestamp
 
-            console.log(numberOfPayments);
+            
             console.log(`Billing Cycle Anchor (Start Date): ${new Date(billing_cycle_anchor * 1000).toISOString()}`);
             console.log(`Cancel At (End Date): ${new Date(cancel_at * 1000).toISOString()}`);
 
